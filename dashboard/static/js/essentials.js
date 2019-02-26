e162729b8646b01a4d15e6a17469d07d4a3bf55c
Biswajee <roy.biswajeet161@gmail.com>
2019-02-26 21:54:10 +0530

    Add JS logic

--- dashboard/static/js/essentials.js
+++ dashboard/static/js/essentials.js
@@ -1,18 +1,27 @@
+var i = 2;
+
+
 function addelement() {
   var maindiv = document.createElement('div');
   maindiv.className = 'form-group';
   var context = document.createElement('div');
   context.className = 'custom-file';
   var inp = document.createElement('input');
+  inp.type = 'file';
+  inp.id = 'fileid'+i;
   inp.className = 'custom-file-input';
   var lb = document.createElement('label');
   lb.className = 'custom-file-label';
+  lb.for = 'fileid'+i;
+  lb.id = 'label-fileid'+i;
+  lb.innerHTML = 'Choose file';
 
   context.appendChild(inp);
   context.appendChild(lb);
-
   maindiv.appendChild(context);
 
   var element = document.getElementById('upload-form');
   element.appendChild(maindiv);
+
+  i = i + 1;
 };
