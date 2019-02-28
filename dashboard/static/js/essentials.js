38858a3944d05cf710dfd071e273284c7fffd515
Biswajee <roy.biswajeet161@gmail.com>
2019-02-28 20:26:56 +0530

    Fully functional

--- dashboard/static/js/essentials.js
+++ dashboard/static/js/essentials.js
@@ -1,28 +1,44 @@
+
+/* Removes all file input div elements to be created using + button */
+
+(function() {
+  for(start=2;start<=6; start++) {
+    document.getElementById('div_id_image_url_' + start).remove();
+  }
+})();
+
+
+
 var i = 2;
 
 
 function addelement() {
-  var maindiv = document.createElement('div');
-  maindiv.className = 'form-group';
-  var context = document.createElement('div');
-  context.className = 'custom-file';
-  var inp = document.createElement('input');
-  inp.type = 'file';
-  inp.name = 'f'+i;
-  inp.id = 'fileid'+i;
-  inp.className = 'custom-file-input';
-  var lb = document.createElement('label');
-  lb.className = 'custom-file-label';
-  lb.for = 'fileid'+i;
-  lb.id = 'label-fileid'+i;
-  lb.innerHTML = 'Choose file';
+  if (i <= 6) {
+    var maindiv = document.createElement('div');
+    maindiv.className = 'form-group';
+    maindiv.id = 'div_id_image_url_' + i;
 
-  context.appendChild(inp);
-  context.appendChild(lb);
-  maindiv.appendChild(context);
+    var lb = document.createElement('label');
+    lb.for = 'div_id_image_url_' + i;
+    lb.className = 'col-form-label';
+    lb.innerHTML = 'Image url ' + i;
 
-  var element = document.getElementById('upload-form');
-  element.appendChild(maindiv);
+    var context = document.createElement('div');
 
-  i = i + 1;
+    var inp = document.createElement('input');
+    inp.type = 'file';
+    inp.name = 'image_url_'+i;
+    inp.className = 'clearablefileinput';
+    inp.id = 'id_image_url_' + i;
+
+
+    context.appendChild(inp);
+    maindiv.appendChild(lb);
+    maindiv.appendChild(context);
+
+    var element = document.getElementById('upload-form');
+    element.appendChild(maindiv);
+
+    i = i + 1;
+  }
 };
