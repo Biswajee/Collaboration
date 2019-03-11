2dec5908e83428f8eee4a9ca09dd93a8e3673bd8
Biswajee <roy.biswajeet161@gmail.com>
2019-03-11 21:49:54 +0530

    Update dashnoard documentation

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -2,6 +2,12 @@ from django import forms
 
 from .models import imgdb
 
+'''
+image_upload form renders on the index.html for image upload service
+and stores associated data to the imgdb database described in the models.py
+'''
+
+
 class image_upload(forms.ModelForm):
     class Meta:
         model = imgdb
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,9 +3,13 @@ from django.core.files.storage import FileSystemStorage
 from .models import imgdb
 from .forms import image_upload
 
+
+# returns the homepage of the application in the base url to access various features of the application
 def index(request):
     return render(request, 'dashboard/entry.html')
 
+# returns a form page for uploading images and stores responses in the form to imgdb database
+# If a get request to the URL is made, a blank form is rendered
 def im_upload(request):
     if request.method == 'POST':
         form = image_upload(request.POST, request.FILES)
@@ -18,9 +22,10 @@ def im_upload(request):
         'form' : form
     })
 
+# pulls out the last entry in the imgdb database and displays the images in the "image_list/" URL
 def gallery_display(request):
     images = imgdb.objects.last()
-    print(type(images))
+    # print(type(images))
     return render(request, 'dashboard/display.html', {
             'images' : images
     })
