f22d641ec8e00f740f98141bd974d728561284fe
Biswajee <roy.biswajeet161@gmail.com>
2021-04-10 09:35:00 +0530

    remove slide viewer from installed apps

--- collaboration/settings.py
+++ collaboration/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dashboard',
     'docviewer',
-    'slideviewer',
+    # 'slideviewer',
     'mediaAPI',
     'visualizations',
     'rest_framework',
