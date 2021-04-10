53c7baec62e3bfe610c67407ae281467886ba446
Biswajee <roy.biswajeet161@gmail.com>
2021-04-10 19:28:01 +0530

    Update settings.py

--- collaboration/settings.py
+++ collaboration/settings.py
@@ -144,6 +144,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
